e0f3518c3ec120610a44927cccba5c3e3829d214
HanseGucker <hansegucker@gmail.com>
2017-08-03 15:10:16 +0200

    New barcode pattern

--- printer.py
+++ printer.py
@@ -12,5 +12,8 @@ class Printing():
         self.pdf.ln(10)
 
         self.pdf.set_font('Arial', 'B', 12)
-        self.pdf.cell(40, 10, 'Great!')
+        self.pdf.cell(40, 10, 'Seniorenticket')
+
+        self.pdf.ln(5)
+        self.pdf.cell(40, 10, '30 EUR', align = "R")
         self.pdf.output('test.pdf', 'F')
