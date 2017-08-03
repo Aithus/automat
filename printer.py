2668eaf9b82b016c9d1d241f68f54214cfbaee37
HanseGucker <hansegucker@gmail.com>
2017-08-03 14:47:50 +0200

    Printer test

---
+++ printer.py
@@ -0,0 +1,16 @@
+class Printing():
+    def __init__(self):
+        self.width = 58
+        self.height = 87
+        from fpdf import FPDF
+        self.pdf = FPDF("P", "mm", [self.width, self.height])
+
+    def create_ticket(self):
+        self.pdf.add_page()
+        self.pdf.set_font('Arial', 'B', 20)
+        self.pdf.cell(40, 10, 'TICKET', align = "C")
+        self.pdf.ln(10)
+
+        self.pdf.set_font('Arial', 'B', 12)
+        self.pdf.cell(40, 10, 'Great!')
+        self.pdf.output('test.pdf', 'F')
---
+++ testprint.py
@@ -0,0 +1,4 @@
+from printer import Printing
+
+test_print = Printing()
+test_print.create_ticket()
