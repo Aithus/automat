1ccca6b056d6f737c66ffea57099ffd4ed78225e
Aithus <lloydmeins@gmx.net>
2017-08-03 13:49:48 +0200

    Merge

--- guis/paymethodcouponui.py
+++ guis/paymethodcouponui.py
@@ -5,11 +5,10 @@ class PayMethodCouponUI(ScanUI):
     def __init__(self, parent = None):
         super().__init__(parent)
         self.init_ui()
-        
+
     def init_ui(self):
 
         self.Labels["information"].setText("Bitte legen Sie ihren Coupon auf das leuchtende Scannerfeld")
-
 
         self.add_cancel_button()
 
